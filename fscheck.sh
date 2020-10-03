0e360d6879932756e72c4e51bc10d3fd41399b16
Sebastian Hriscu <64442981+sebastian105@users.noreply.github.com>
2020-10-02 18:44:07 -0700

    Update fscheck.sh

--- fscheck.sh
+++ fscheck.sh
@@ -1,4 +1,15 @@
-
+SVENDOR=/mnt/vendora2
+SSYSTEM=/mnt/systema2
+PVENDOR=/mnt/vendorport
+PSYSTEM=/mnt/systemport
+CURRENTUSER=$3
+SCRIPTDIR=$(readlink -f "$0")
+CURRENTDIR=$(dirname "$SCRIPTDIR")
+FILES=$CURRENTDIR/files
+PORTZIP=$1
+STOCKTAR=$2
+OUTP=$CURRENTDIR/out
+TOOLS=$CURRENTDIR/tools
 echo "Scanning system for errors"
 e2fsck -y -f $OUTP/systemport.img
 echo "Resizing system"
