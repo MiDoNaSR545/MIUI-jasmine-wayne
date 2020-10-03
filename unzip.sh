ad510110123c49046ea5dc0ff90819dc6e0be5c8
Sebastian Hriscu <64442981+sebastian105@users.noreply.github.com>
2020-10-02 20:57:58 -0700

    Update unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -2,12 +2,11 @@ SVENDOR=/mnt/vendora2
 SSYSTEM=/mnt/systema2
 PVENDOR=/mnt/vendorport
 PSYSTEM=/mnt/systemport
-CURRENTUSER=$3
+CURRENTUSER=$2
 SCRIPTDIR=$(readlink -f "$0")
 CURRENTDIR=$(dirname "$SCRIPTDIR")
 FILES=$CURRENTDIR/files
 PORTZIP=$1
-STOCKTAR=$2
 OUTP=$CURRENTDIR/out
 TOOLS=$CURRENTDIR/tools
 
