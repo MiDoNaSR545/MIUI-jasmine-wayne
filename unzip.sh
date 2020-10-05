43a21b26fb39d0aae733509caac923b7e7cbb52d
Sebastian Hriscu <64442981+sebastian105@users.noreply.github.com>
2020-10-05 08:51:45 -0700

    Update unzip.sh

--- unzip.sh
+++ unzip.sh
@@ -9,22 +9,18 @@ FILES=$CURRENTDIR/files
 PORTZIP=$1
 OUTP=$CURRENTDIR/out
 TOOLS=$CURRENTDIR/tools
-
 cd ~
 #sudo pacman --noconfirm --noedit -Syu trizen cpio brotli abootimg
 #trizen --noconfirm --noedit -Syu simg-tools aosp-devel
 sudo apt -y install cpio brotli simg2img abootimg git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip zip screen attr ccache libssl-dev imagemagick schedtool
-
 echo "Fail on all errors enabled"
 set -e
-
 echo "Removing $OUTP"
 rm -rf $OUTP || true
 mkdir $OUTP
 chown $CURRENTUSER:$CURRENTUSER $OUTP
 echo "Copying zip to $OUTP"
 cp -Raf $CURRENTDIR/zip $OUTP/
-
 echo "Unzipping $PORTZIP"
 unzip -d $OUTP $PORTZIP system.transfer.list vendor.transfer.list system.new.dat.br vendor.new.dat.br
 echo "Decompressing port system.new.dat.br"
@@ -37,17 +33,16 @@ echo "Converting port vendor.new.dat to disk image"
 $TOOLS/sdat2img/sdat2img.py $OUTP/vendor.transfer.list $OUTP/vendor.new.dat $OUTP/vendorport.img
 echo "Cleaning up unnecessary files from $OUTP"
 rm $OUTP/vendor.img $OUTP/system.img $OUTP/system.new.dat $OUTP/vendor.new.dat $OUTP/system.transfer.list $OUTP/vendor.transfer.list
-
 echo "Making directories"
-mkdir $PSYSTEM 
-mkdir $PVENDOR 
-mkdir $SVENDOR 
-mkdir $SSYSTEM 
+mkdir $PSYSTEM || true
+mkdir $PVENDOR || true
+mkdir $SVENDOR || true
+mkdir $SSYSTEM || true
 echo "Mounting port system to $PSYSTEM"
-mount -o rw,noatime $OUTP/systemport.img $PSYSTEM
+sudo mount -o rw,noatime $OUTP/systemport.img $PSYSTEM
 echo "Mounting port vendor to $PVENDOR"
-mount -o rw,noatime $OUTP/vendorport.img $PVENDOR
+sudo mount -o rw,noatime $OUTP/vendorport.img $PVENDOR
 echo "Mounting source system to $SSYSTEM"
-mount -o rw,noatime /home/sebastian1/systema2.img $SSYSTEM
+sudo mount -o rw,noatime /home/sebastian1/systema2.img $SSYSTEM
 echo "Mounting source vendor to $SVENDOR"
-mount -o rw,noatime /home/sebastian1/vendora2.img $SVENDOR
+sudo mount -o rw,noatime /home/sebastian1/vendora2.img $SVENDOR
