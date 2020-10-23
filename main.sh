9f7f7412c3281f076f17c47d82fb586b31728ed2
sebastian467 <shriscu2000@gmail.com>
2020-10-22 23:49:23 -0700

    Reorganize and debloat more apps

--- main.sh
+++ main.sh
@@ -6,23 +6,25 @@ CURRENTUSER=$2
 SCRIPTDIR=$(readlink -f "$0")
 CURRENTDIR=$(dirname "$SCRIPTDIR")
 FILES=$CURRENTDIR/files
-PORTZIP=$1
 OUTP=$CURRENTDIR/out
 TOOLS=$CURRENTDIR/tools
+PORTZIP=$1
+ANALYTICS=$CURRENTDIR/analytics
 
-# INIT
-sudo apt -y install figlet > /dev/null 2>&1
+# Prepare directories and files
+mkdir $ANALYTICS
+sudo apt -y install figlet > $ANALYTICS/MIUI-jasmeme.log 2>&1
 figlet MIUI-jasmeme
 echo " "
 echo "• Updating submodules"
-sudo apt -y update > /dev/null 2>&1
-sudo apt -y upgrade > /dev/null 2>&1
-sudo apt -y install cpio brotli simg2img img2simg abootimg git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip zip screen attr ccache libssl-dev schedtool > /dev/null 2>&1
-git clone https://github.com/xpirt/img2sdat $TOOLS/img2sdat
-git clone https://github.com/xpirt/sdat2img $TOOLS/sdat2img
+sudo apt -y update > $ANALYTICS/MIUI-jasmeme.log 2>&1
+sudo apt -y upgrade > $ANALYTICS/MIUI-jasmeme.log 2>&1
+sudo apt -y install cpio brotli simg2img img2simg abootimg git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip zip screen attr ccache libssl-dev schedtool > $ANALYTICS/MIUI-jasmeme.log 2>&1
+git clone https://github.com/xpirt/img2sdat $TOOLS/img2sdat > $ANALYTICS/MIUI-jasmeme.log 2>&1
+git clone https://github.com/xpirt/sdat2img $TOOLS/sdat2img > $ANALYTICS/MIUI-jasmeme.log 2>&1
 echo "• Beginning port sequence"
 
-# JASMINE GLOBAL IMAGES
+# Unzip jasmine_global_images.tgz if needed
 read -p "• Unzip jasmine_global_images? [Y/N]" JSAM
 if [[ "$JSAM" = "Y" ]]; then
 read -p "• Enter your jasmine_global_images filename: " STOCKTAR
@@ -37,17 +39,17 @@ echo "• Converting vendor image to EXT4 image"
 simg2img $OUTP/vendor.img $CURRENTDIR/vendora2.img
 fi
 
-# UNZIP
+# Unzip and convert base ROM to disk images
 echo "• Unzipping $PORTZIP"
-unzip -d $OUTP $PORTZIP system.transfer.list vendor.transfer.list system.new.dat.br vendor.new.dat.br > /dev/null 2>&1
+unzip -d $OUTP $PORTZIP system.transfer.list vendor.transfer.list system.new.dat.br vendor.new.dat.br > $ANALYTICS/MIUI-jasmeme.log 2>&1
 echo "• Decompressing port system.new.dat.br"
 brotli -j -v -d $OUTP/system.new.dat.br -o $OUTP/system.new.dat
 echo "• Decompressing port vendor.new.dat.br"
 brotli -j -v -d $OUTP/vendor.new.dat.br -o $OUTP/vendor.new.dat
 echo "• Converting port systm.new.dat to disk image"
-$TOOLS/sdat2img/sdat2img.py $OUTP/system.transfer.list $OUTP/system.new.dat $OUTP/systemport.img > /dev/null 2>&1
+$TOOLS/sdat2img/sdat2img.py $OUTP/system.transfer.list $OUTP/system.new.dat $OUTP/systemport.img > $ANALYTICS/MIUI-jasmeme.log 2>&1
 echo "• Converting port vendor.new.dat to disk image"
-$TOOLS/sdat2img/sdat2img.py $OUTP/vendor.transfer.list $OUTP/vendor.new.dat $OUTP/vendorport.img > /dev/null 2>&1
+$TOOLS/sdat2img/sdat2img.py $OUTP/vendor.transfer.list $OUTP/vendor.new.dat $OUTP/vendorport.img > $ANALYTICS/MIUI-jasmeme.log 2>&1
 echo "• Cleaning up unnecessary files from $OUTP"
 rm -rf $OUTP/system.new.dat $OUTP/vendor.new.dat $OUTP/system.transfer.list $OUTP/vendor.transfer.list
 echo "• Making directories"
@@ -64,23 +66,36 @@ sudo mount -o rw,noatime systema2.img $SSYSTEM
 echo "•Mounting source vendor to $SVENDOR"
 sudo mount -o rw,noatime vendora2.img $SVENDOR
 
-# PORT
-echo "• Creating addon d"
+# Fix Magisk and cam watermarks
+echo "• Creating Magisk addon"
 mkdir $PSYSTEM/system/addon.d
 setfattr -h -n security.selinux -v u:object_r:system_file:s0 $PSYSTEM/system/addon.d
 chmod 755 $PSYSTEM/system/addon.d
-echo "• Patching watermarks"
+echo "• Patching camera watermarks"
 cp -af $SVENDOR/etc/MIUI_DualCamera_watermark.png $PVENDOR/etc/MIUI_DualCamera_watermark.png
-echo "• Removing updater"
+
+#Debloat MIUI and Google Trash @clarencejasmeme
+echo "• Debloating unnecessary apps"
 rm -rf $PSYSTEM/system/priv-app/Updater
-echo "• Renaming device features xmls"
+rm -rf $PSYSTEM/system/priv-app/WfdService
+rm -rf $PSYSTEM/system/priv-app/MiRecycle
+rm -rf $PSYSTEM/system/priv-app/MiService
+rm -rf $PSYSTEM/system/app/AutoTest
+rm -rf $PSYSTEM/system/app/MiuiBugReport
+rm -rf $PSYSTEM/system/app/wps_lite
+rm -rf $PSYSTEM/system/app/Joyose
+rm -rf $PSYSTEM/system/app/Health
+rm -rf $PSYSTEM/system/app/Qmmi
+
+# Edit build.prop to Mi 6X specs
+echo "• Renaming device features XMLs"
 mv $PSYSTEM/system/etc/device_features/lavender.xml $PSYSTEM/system/etc/device_features/wayne.xml
 mv $PVENDOR/etc/device_features/lavender.xml $PVENDOR/etc/device_features/wayne.xml
-echo "• Editing build prop"
+echo "• Editing build prop files"
 sed -i "/persist.camera.HAL3.enabled=/c\persist.camera.HAL3.enabled=1
 /persist.vendor.camera.HAL3.enabled=/c\persist.vendor.camera.HAL3.enabled=1
 /ro.product.model=/c\ro.product.model=MI 6X
-/ro.build.id=/c\ro.build.id=MIUI 12
+/ro.build.id=/c\ro.build.id=10 MIUI 12 by Sebastian
 /persist.vendor.camera.exif.model=/c\persist.vendor.camera.exif.model=MI 6X
 /ro.product.name=/c\ro.product.name=wayne
 /ro.product.device=/c\ro.product.device=wayne
@@ -94,37 +109,70 @@ sed -i "/persist.camera.HAL3.enabled=/c\persist.camera.HAL3.enabled=1
 \$ i sys.tianma_nt36672_length=46
 \$ i sys.jdi_nt36672_offset=9
 \$ i sys.jdi_nt36672_length=45
+\$ i persist.vendor.imx376_sunny.low.lux=310
+\$ i persist.vendor.imx376_sunny.light.lux=280
+\$ i persist.vendor.imx376_ofilm.low.lux=310
+\$ i persist.vendor.imx376_ofilm.light.lux=280
+\$ i persist.vendor.bokeh.switch.lux=290
+\$ i persist.vendor.camera.auxswitch.threshold=330
+\$ i persist.vendor.camera.mainswitch.threshold=419
+\$ i persist.vendor.camera.stats.test=0
+\$ i persist.vendor.camera.depth.focus.cb=0
+\$ i persist.vendor.camera.isp.clock.optmz=0
+\$ i persist.vendor.camera.linkpreview=0
+\$ i persist.vendor.camera.auxswitch.threshold=330
+\$ i persist.vendor.camera.mainswitch.threshold=419
+\$ i persist.vendor.camera.expose.aux=1
 /persist.vendor.camera.model=/c\persist.vendor.camera.model=MI 6X" $PSYSTEM/system/build.prop
 sed -i "/ro.build.characteristics=/c\ro.build.characteristics=nosdcard" $PSYSTEM/system/product/build.prop
 sed -i "/ro.product.vendor.model=/c\ro.product.vendor.model=MI 6X
 /ro.product.vendor.name=/c\ro.product.vendor.name=wayne
+\$ i persist.vendor.imx376_sunny.low.lux=310
+\$ i persist.vendor.imx376_sunny.light.lux=280
+\$ i persist.vendor.imx376_ofilm.low.lux=310
+\$ i persist.vendor.imx376_ofilm.light.lux=280
+\$ i persist.vendor.bokeh.switch.lux=290
+\$ i persist.vendor.camera.auxswitch.threshold=330
+\$ i persist.vendor.camera.mainswitch.threshold=419
+\$ i persist.vendor.camera.stats.test=0
+\$ i persist.vendor.camera.depth.focus.cb=0
+\$ i persist.vendor.camera.isp.clock.optmz=0
+\$ i persist.vendor.camera.linkpreview=0
+\$ i persist.vendor.camera.auxswitch.threshold=330
+\$ i persist.vendor.camera.mainswitch.threshold=419
+\$ i persist.vendor.camera.expose.aux=1
 /ro.product.vendor.device=/c\ro.product.vendor.device=wayne" $PVENDOR/build.prop
 sed -i "/ro.product.odm.device=/c\ro.product.odm.device=wayne
 /ro.product.odm.model=/c\ro.product.odm.model=MI 6X
 /ro.product.odm.device=/c\ro.product.odm.device=wayne
+/ro.product.model=/c\ro.product.model=MI 6X
 /ro.product.odm.name=/c\ro.product.odm.name=wayne" $PVENDOR/odm/etc/build.prop
-echo "• Patching firmware"
+
+# Firmware / fstabs
+echo "• Patching vendor firmware"
 rm -rf $PVENDOR/firmware
-echo "• Patching fstab"
+cp -Raf $SVENDOR/firmware $PVENDOR/firmware
 #VENDOR
+echo "• Copying fstabs"
 cp -f $FILES/fstab.qcom $PVENDOR/etc/
 chmod 644 $PVENDOR/etc/fstab.qcom
 setfattr -h -n security.selinux -v u:object_r:vendor_configs_file:s0 $PVENDOR/etc/fstab.qcom
 chown -hR root:root $PVENDOR/etc/fstab.qcom
-echo "• Patching init"
 #KEYMASTER
+echo "• Keymaster edit"
 rm -f $PVENDOR/etc/init/android.hardware.keymaster@4.0-service-qti.rc
 cp -af $SVENDOR/etc/init/android.hardware.keymaster@3.0-service-qti.rc $PVENDOR/etc/init/android.hardware.keymaster@3.0-service-qti.rc
-
 sed -i "171 s/        <version>4.0<\/version>/        <version>3.0<\/version>/g
 s/4.0::IKeymasterDevice/3.0::IKeymasterDevice/g" $PVENDOR/etc/vintf/manifest.xml
-echo "• Parsing sensors"
+
+# Add vendor sensors / libs
+echo "• Fixing vendor sensors"
 rm -rf $PVENDOR/etc/sensors
 cp -Raf $SVENDOR/etc/sensors $PVENDOR/etc/sensors
 cp -af $SVENDOR/etc/camera/camera_config.xml $PVENDOR/etc/camera/camera_config.xml
 cp -af $SVENDOR/etc/camera/csidtg_camera.xml $PVENDOR/etc/camera/csidtg_camera.xml
 cp -af $SVENDOR/etc/camera/csidtg_chromatix.xml $PVENDOR/etc/camera/camera_chromatix.xml
-echo "• Reparsing cam libs"
+echo "• Parsing vendor libs"
 cp -af $SVENDOR/lib/libMiWatermark.so $PVENDOR/lib/libMiWatermark.so
 cp -af $SVENDOR/lib/libdng_sdk.so $PVENDOR/lib/libdng_sdk.so
 cp -af $SVENDOR/lib/libvidhance_gyro.so $PVENDOR/lib/libvidhance_gyro.so
@@ -132,7 +180,9 @@ cp -af $SVENDOR/lib/libvidhance.so $PVENDOR/lib/
 cp -af $SVENDOR/lib/libmmcamera* $PVENDOR/lib/
 cp -af $SVENDOR/lib64/libmmcamera* $PVENDOR/lib64/
 cp -f $SVENDOR/lib/hw/camera.sdm660.so $PVENDOR/lib/hw/
-echo "• Patching device fingerprint"
+
+# Patch vendor fingerprint files
+echo "• Making device fingerprint"
 cp -af $FILES/fingerprint/app/FingerprintExtensionService/FingerprintExtensionService.apk $PVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk
 setfattr -h -n security.selinux -v u:object_r:vendor_app_file:s0 $PVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk
 chmod 644 $PVENDOR/app/FingerprintExtensionService/FingerprintExtensionService.apk
@@ -173,12 +223,14 @@ cp -af $FILES/fingerprint/lib64/libgf_hal.so $PVENDOR/lib64/libgf_hal.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib64/libgf_hal.so
 chmod 644 $PVENDOR/lib64/libgf_hal.so
 chown -hR root:root $PVENDOR/lib64/libgf_hal.so
-echo "• Patching keylayout"
+
+# Port system biometrics and keylayout
+echo "• Fixing keylayout"
 cp -af $SSYSTEM/system/usr/keylayout/uinput-fpc.kl $PSYSTEM/system/usr/keylayout/uinput-fpc.kl
 cp -af $SSYSTEM/system/usr/idc/uinput-fpc.idc $PSYSTEM/system/usr/idc/uinput-fpc.idc
 cp -af $SSYSTEM/system/usr/keylayout/uinput-fpc.kl $PSYSTEM/system/usr/keylayout/uinput-fpc.kl
 cp -af $SSYSTEM/system/usr/idc/uinput-fpc.idc $PSYSTEM/system/usr/idc/uinput-fpc.idc
-echo "• Recognizing goodixbiometrics"
+echo "• Editing goodix biometrics"
 sed -i "467 c\        <name>vendor.goodix.hardware.fingerprint</name>" $PVENDOR/etc/vintf/manifest.xml
 sed -i "469 c\        <version>1.0</version>
 471 c\            <name>IGoodixBiometricsFingerprint</name>
@@ -188,18 +240,23 @@ sed -i "469 c\        <version>1.0</version>
 477d
 478d
 479d" $PVENDOR/etc/vintf/manifest.xml
+
+# Add libs / firmware from source 
+echo "• Patching system firmware"
 rm -rf $PSYSTEM/system/etc/firmware || true
-echo "• Patching wifi HAL 64"
+cp -Raf $SSYSTEM/system/etc/firmware/* $PVENDOR/firmware/ || true
+echo "• Patching wifi HALs"
 cp -f $FILES/libwifi-hal64.so $PVENDOR/lib64/libwifi-hal.so
 chmod 644 $PVENDOR/lib64/libwifi-hal.so
 chown -hR root:root $PVENDOR/lib64/libwifi-hal.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib64/libwifi-hal.so
-echo "• Patching wifi HAL 32"
 cp -f $FILES/libwifi-hal32.so $PVENDOR/lib/libwifi-hal.so
 chmod 644 $PVENDOR/lib/libwifi-hal.so
 chown -hR root:root $PVENDOR/lib/libwifi-hal.so
 setfattr -h -n security.selinux -v u:object_r:vendor_file:s0 $PVENDOR/lib/libwifi-hal.so
-echo "• Editining system features"
+
+# Edit wayne.xml from /system/etc/device_features
+echo "• Editing system device features"
 #system/etc/device_features
 sed -i "/support_dual_sd_card/c\    <bool name=\"support_dual_sd_card\">true<\/bool>
 /battery_capacity_typ/c\    <string name=\"battery_capacity_typ\">3010<\/string>
@@ -209,8 +266,9 @@ sed -i "/support_dual_sd_card/c\    <bool name=\"support_dual_sd_card\">true<\/b
 /is_redmi/c\    <bool name=\"is_redmi\">false<\/bool>
 /paper_mode_max_level/c\    <float name=\"paper_mode_max_level\">32.0<\/float>
 /paper_mode_min_level/c\    <float name=\"paper_mode_min_level\">0.0<\/float>
+\$ i <bool name="support_aod">true</bool> 
 /is_18x9_ratio_screen/c\    <bool name=\"is_18x9_ratio_screen\">true<\/bool>" $PSYSTEM/system/etc/device_features/wayne.xml
-echo "• Editing vendor features"
+echo "• Editing vendor device features"
 #vendor/etc/device_features
 sed -i "/support_dual_sd_card/c\    <bool name=\"support_dual_sd_card\">true<\/bool>
 /battery_capacity_typ/c\    <string name=\"battery_capacity_typ\">3010<\/string>
@@ -220,13 +278,17 @@ sed -i "/support_dual_sd_card/c\    <bool name=\"support_dual_sd_card\">true<\/b
 /is_redmi/c\    <bool name=\"is_redmi\">false<\/bool>
 /paper_mode_max_level/c\    <float name=\"paper_mode_max_level\">32.0<\/float>
 /paper_mode_min_level/c\    <float name=\"paper_mode_min_level\">0.0<\/float>
+\$ i <bool name="support_aod">true</bool> 
 /is_18x9_ratio_screen/c\    <bool name=\"is_18x9_ratio_screen\">true<\/bool>" $PVENDOR/etc/device_features/wayne.xml
-echo "• Patching audio"
-#AUDIO
+
+#Audio fix
+echo "• Fixing audio"
 rm -rf $PVENDOR/etc/acdbdata
 cp -Raf $SVENDOR/etc/acdbdata $PVENDOR/etc/acdbdata
-echo "• Adding overlays"
-#statusbar/corner
+
+
+# Overlays
+echo "• Patching overlays and notch"
 rm -rf $PVENDOR/app/NotchOverlay
 cp -f $FILES/overlay/DevicesOverlay.apk $PVENDOR/overlay/DevicesOverlay.apk
 cp -f $FILES/overlay/DevicesAndroidOverlay.apk $PVENDOR/overlay/DevicesAndroidOverlay.apk
@@ -236,8 +298,9 @@ chown -hR root:root $PVENDOR/overlay/DevicesOverlay.apk
 chown -hR root:root $PVENDOR/overlay/DevicesAndroidOverlay.apk
 setfattr -h -n security.selinux -v u:object_r:vendor_overlay_file:s0 $PVENDOR/overlay/DevicesOverlay.apk
 setfattr -h -n security.selinux -v u:object_r:vendor_overlay_file:s0 $PVENDOR/overlay/DevicesAndroidOverlay.apk
+
+# Reading mode
 echo "• Fixing reading mode"
-#readingmode 
 cp -f $FILES/readingmode/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
 cp -f $FILES/readingmode/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml $PVENDOR/etc/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml
 chmod 644 $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
@@ -246,6 +309,9 @@ chown -hR root:root $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_
 chown -hR root:root $PVENDOR/etc/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml
 setfattr -h -n security.selinux -v u:object_r:vendor_configs_file:s0 $PVENDOR/etc/qdcm_calib_data_jdi_nt36672_fhd_video_mode_dsi_panel.xml
 setfattr -h -n security.selinux -v u:object_r:vendor_configs_file:s0 $PVENDOR/etc/qdcm_calib_data_tianma_nt36672_fhd_video_mode_dsi_panel.xml
+
+# Make last edits
+echo "• Editing wifi firmware"
 sed -i "124 i \
 124 i \    # Wifi firmware reload path
 124 i \    chown wifi wifi /sys/module/wlan/parameters/fwpath
@@ -255,12 +321,14 @@ sed -i "124 i \
 124 i \    chown system system /sys/touchpanel/double_tap" $PVENDOR/etc/init/hw/init.target.rc
 read -p "• Done editing port [ENTER to continue]"
 
-# ZIP
+# Edit updater-script
 echo "• Fetching ROM info"
 ROMVERSION=$(sudo grep ro.system.build.version.incremental= $PSYSTEM/system/build.prop | sed "s/ro.system.build.version.incremental=//g"; )
 echo "• Patching updater-script"
 sed -i "s%DATE%$(date +%d/%m/%Y)%g
 s/ROMVERSION/$ROMVERSION/g" $OUTP/zip/META-INF/com/google/android/updater-script
+
+# Unmount / cleanup
 echo "• Unmounting port system"
 sudo umount $PSYSTEM
 echo "• Unmounting port vendor"
@@ -274,21 +342,25 @@ sudo rmdir $PSYSTEM
 sudo rmdir $PVENDOR
 sudo rmdir $SSYSTEM
 sudo rmdir $SVENDOR
+
+# Check and resize system
 echo "• Checking filesystems"
-e2fsck -y -f $OUTP/systemport.img > /dev/null 2>&1
+e2fsck -y -f $OUTP/systemport.img > $ANALYTICS/MIUI-jasmeme.log 2>&1
 echo "• Resizing system to 3.0 G"
-resize2fs $OUTP/systemport.img 786432 > /dev/null 2>&1
+resize2fs $OUTP/systemport.img 786432 > $ANALYTICS/MIUI-jasmeme.log 2>&1
+
+# Zip the final ROM
 echo "• Converting port system to sparse image"
 img2simg $OUTP/systemport.img $OUTP/sparsesystem.img
 rm $OUTP/systemport.img
 echo "• Generating DAT files for system"
-$TOOLS/img2sdat/img2sdat.py -v 4 -o $OUTP/zip -p system $OUTP/sparsesystem.img > /dev/null 2>&1
+$TOOLS/img2sdat/img2sdat.py -v 4 -o $OUTP/zip -p system $OUTP/sparsesystem.img > $ANALYTICS/MIUI-jasmeme.log 2>&1
 rm $OUTP/sparsesystem.img
 echo "• Converting port vendor to sparse image"
 img2simg $OUTP/vendorport.img $OUTP/sparsevendor.img
 rm $OUTP/vendorport.img
 echo "• Generating DAT files for vendor"
-$TOOLS/img2sdat/img2sdat.py -v 4 -o $OUTP/zip -p vendor $OUTP/sparsevendor.img > /dev/null 2>&1
+$TOOLS/img2sdat/img2sdat.py -v 4 -o $OUTP/zip -p vendor $OUTP/sparsevendor.img > $ANALYTICS/MIUI-jasmeme.log 2>&1
 rm $OUTP/sparsevendor.img
 echo "• Compressing system.new.dat"
 brotli -j -v -q 6 $OUTP/zip/system.new.dat
@@ -297,8 +369,10 @@ brotli -j -v -q 6 $OUTP/zip/vendor.new.dat
 cp -af $FILES/boot.img $OUTP/zip
 cd $OUTP/zip
 echo "• Zipping final ROM"
-zip -ry $OUTP/10_MIUI_12_jasmine_sprout_$ROMVERSION.zip * > /dev/null 2>&1
+zip -ry $OUTP/10_MIUI_12_wayne_$ROMVERSION.zip * > $ANALYTICS/MIUI-jasmeme.log 2>&1
 cd $CURRENTDIR
+
+# Cleanup
 echo "• Removing all unnecessary files"
 rm -rf $OUTP/zip
 chown -hR $CURRENTUSER:$CURRENTUSER $OUTP
